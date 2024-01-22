0d123bd5489240793e6c7fb9925a1bee1f36bef2
Zhytou <zhoytou@gmail.com>
2024-01-22 23:40:35 +0800

    feat: add more comment and extract star_id from create_star_image output, also fix the rotation bug

--- generate.py
+++ generate.py
@@ -22,34 +22,44 @@ def generate_dataset(num_img: int, w: int=256, l: int=256):
 
     # generate the star image
     for i in range(num_img):
-        img, _ = create_star_image(69, -12, -13)
+        # star_info is a list of [star_ids[i], (row, col), star_magnitudes[i]]
+        img, star_info = create_star_image(69, -12, -13)
+        # generate star_table: (row, col) -> star_id
+        star_table = dict(map(lambda x: (x[1], x[0]), star_info))
 
         # get the centroids of the stars in the image
         stars = get_star_centroids(img)
+        assert(len(stars) == len(star_info))
         # data structure for fast nearest neighbour search
         tree = KDTree(stars)
 
         stars = np.array(stars)
-        # choose a guide star as the reference star, and find the nearest star to it
+        # choose a guide star as the reference star
         for guide_star in stars:
+            # check if false star
+            star_id = star_table.get(tuple(guide_star), -1)
+            if star_id == -1:
+                continue
+            # check if the guide star is too close to the edge
             if guide_star[0] - w/2 < 0 or guide_star[0] + w/2 > img.shape[0] or guide_star[1] - l/2 < 0 or guide_star[1] + l/2 > img.shape[1]:
                 continue
+            
+            # find the nearest neighbor star to the reference one
             _, idxs = tree.query(guide_star, 2)
             nearest_star = stars[idxs[1]]
-            
+
             # calculate rotation angle & matrix
-            angle = degrees(atan((nearest_star[1] - guide_star[1]) / (nearest_star[0] - guide_star[0])))
-            M = cv2.getRotationMatrix2D(guide_star, angle, 1)
+            angle = degrees(atan((nearest_star[0] - guide_star[0]) / (nearest_star[1] - guide_star[1])))
+            # be careful getRotationMatrix2D takes (width, height) as input, in other words (col, row)
+            M = cv2.getRotationMatrix2D((float(guide_star[1]), float(guide_star[0])), angle, 1)
             rotated_img = cv2.warpAffine(img, M, img.shape)
 
             # crop and save the image
             row1, row2 = int(guide_star[0] - w/2), int(guide_star[0] + w/2)
             col1, col2 = int(guide_star[1] - l/2), int(guide_star[1] + l/2)
 
-            cv2.imwrite(f'{i}.png', rotated_img)
-            # cv2.imwrite(f'{i}.png', rotated_img[row1:row2, col1:col2])
-        # generate label information for training
-
+            # generate label information for training and testing
+            cv2.imwrite(f'{star_id}.png', rotated_img[row1:row2, col1:col2])
 
 
 
