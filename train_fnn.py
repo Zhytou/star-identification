3110436610b063ce1df2a48bdd90aa2b38b36f9f
Zhytou <zhoytou@gmail.com>
2024-02-02 18:01:35 +0800

    feat: update 1、增加使用gpu训练的代码； 2、将train和check_accuracy分别用函数包装； 3、增加validate部分，用于调优超参数。

--- train_fnn.py
+++ train_fnn.py
@@ -1,29 +1,16 @@
 import torch
-import torch.nn as nn
-from torch.utils.data import DataLoader as dataloader, random_split
+import torch.nn as nn, torch.optim as optim
+import torch.nn.functional as F
+from torch.utils.data import DataLoader, random_split
 
 from dataset import StarPointDataset
 from generate import star_num_per_sample, num_classes
 
 
-# training setting
-batch_size = 10
-n_iters = 6000
-learning_rate = 0.1
-
-# define datasets for training & validation
-dataset = StarPointDataset('data/star_points/labels.csv')
-train_dataset, test_dataset = random_split(dataset, [0.9, 0.1])
-
-# create data loaders for our datasets
-train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
-
-# print split sizes
-print(f'Training set has {len(train_dataset)} instances')
-
 class FeedforwardNeuralNetModel(nn.Module):
-    # model structure refers to https://www.mdpi.com/1424-8220/20/13/3684
+    '''
+        The feedforward neural network model. By the way, model structure refers to https://www.mdpi.com/1424-8220/20/13/3684.
+    '''
     def __init__(self, input_dim: int, output_dim: int, hidden_dims: list[int] = [300, 100]):
         super(FeedforwardNeuralNetModel, self).__init__()
         self.layer = nn.Sequential(
@@ -42,43 +29,102 @@ class FeedforwardNeuralNetModel(nn.Module):
     def forward(self, x):
         out = self.layer(x)
         return out
+
+
+def train(model: nn.Module, optimizer: optim.Optimizer, num_epochs: int, loader: DataLoader, test_loader: DataLoader=None):
+    '''
+        Train the model.
+    Args:
+        model: the model to be trained
+        optimizer: the optimizer
+        num_epochs: the number of epochs
+        loader: the data loader for training
+        test_loader: the data loader for testing
+    '''
+    for epoch in range(num_epochs):
+        for points, labels in loader:
+            # points.to(device)
+            # labels.to(device)
+            # print(points.shape, points.device, points.is_cuda)
+            # print(next(model.parameters()).device) 
+
+            # Clear gradients w.r.t. parameters
+            optimizer.zero_grad()
+            # Forward pass to get output/logits
+            scores = model(points)
+            # Calculate Loss: softmax --> cross entropy loss
+            loss = F.cross_entropy(scores, labels)
+            # Getting gradients w.r.t. parameters
+            loss.backward()
+            # Updating parameters
+            optimizer.step()
+        
+        if test_loader:
+            accuracy = check_accuracy(model, test_loader)
+            print(f'Epoch: {epoch}, Accuracy: {accuracy}')
+        
+        
+def check_accuracy(model: nn.Module, loader: DataLoader):
+    '''
+        Check the accuracy of the model.
+    Args:
+        model: the model to be checked
+        loader: the data loader for validation or testing
+    '''
+    # Calculate Accuracy         
+    correct = 0
+    total = 0
+    # Iterate through test dataset
+    for points, labels in loader:
+        # points.to(device)
+        # labels.to(device)
+        # Forward pass only to get logits/output
+        outputs = model(points)
+        # Get predictions from the maximum value
+        _, predicted = torch.max(outputs.data, 1)
+        # Total number of labels
+        total += batch_size
+        # Total correct predictions
+        correct += (predicted == labels).sum().item()
+    return round(100.0 * correct / total, 2)
+
+
+if __name__ == '__main__':
+    # training setting
+    batch_size = 100
+    n_iters = 6000
+    learning_rate = 0.1
+    # use gpu if available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(f'Using device: {device}')
+
+    # define datasets for training & validation
+    dataset = StarPointDataset('data/star_points/Filtered_Below_5.7_SAO/labels.csv')
+    train_dataset, validate_dataset, test_dataset = random_split(dataset, [0.7, 0.2, 0.1])
+    # print split sizes
+    print(f'Training set: {len(train_dataset)}, Validation set: {len(validate_dataset)}, Test set: {len(test_dataset)}')
+
+    # create data loaders for our datasets
+    train_loader = DataLoader(train_dataset, batch_size, shuffle=True)
+    validate_loader = DataLoader(validate_dataset, batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size, shuffle=False)
+
+    num_epochs = int(n_iters / (len(train_dataset) / batch_size))
+    hidden_dimss = [[20, 10], [40, 20], [60, 30]]
+    best_model = None
+    best_accuracy = 0.0
+    for hidden_dims in hidden_dimss:  
+        model = FeedforwardNeuralNetModel(star_num_per_sample*2, num_classes)
+        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  
+
+        train(model, optimizer, num_epochs, train_loader)
+        val_accuracy = check_accuracy(model, validate_loader)
+        print(val_accuracy)
+        if val_accuracy > best_accuracy:
+            best_model = model
+            best_accuracy = val_accuracy
     
-
-model = FeedforwardNeuralNetModel(star_num_per_sample * 2, num_classes)
-criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)  
-
-num_epochs = n_iters / (len(train_dataset) / batch_size)
-num_epochs = int(num_epochs)
-
-iter = 0
-for epoch in range(num_epochs):
-    for points, labels in train_loader:
-        # Clear gradients w.r.t. parameters
-        optimizer.zero_grad()
-        # Forward pass to get output/logits
-        scores = model(points)
-        # Calculate Loss: softmax --> cross entropy loss
-        loss = criterion(scores, labels)
-        # Getting gradients w.r.t. parameters
-        loss.backward()
-        # Updating parameters
-        optimizer.step()
-        iter += 1
-        if iter % 500 == 0:
-            # Calculate Accuracy         
-            correct = 0
-            total = 0
-            # Iterate through test dataset
-            for points, labels in test_loader:
-                # Forward pass only to get logits/output
-                outputs = model(points)
-                # Get predictions from the maximum value
-                _, predicted = torch.max(outputs.data, 1)
-                # Total number of labels
-                total += batch_size
-                # Total correct predictions
-                correct += (predicted == labels).sum().item()
-            accuracy = round(100.0 * correct / total, 2)
-            # Print Loss
-            print(f'Iteration: {iter}. Loss: {loss.item()}. Accuracy: {accuracy}%')
+    print(f'Best validate accuracy: {best_accuracy}')
+    test_accuray = check_accuracy(best_model, test_loader)
+    print(f'Test accuracy: {test_accuray}')
+    torch.save(best_model.state_dict(), 'model/fnn_model.pth')
