1b29c82de02290530a4640619fad2e548ee9d278
Zhytou <zhoytou@gmail.com>
2024-03-18 15:52:45 +0800

    feat: remove generate_img_dataset & add pattern match database generation(grid algorithmn)

--- generate.py
+++ generate.py
@@ -1,144 +1,164 @@
 import os
 import uuid
-from datetime import datetime
-import cv2
+from math import degrees
 import numpy as np
 import pandas as pd
-from math import degrees, atan
 from concurrent.futures import ThreadPoolExecutor
-from scipy.spatial import KDTree
 from scipy.spatial.distance import cdist
 
 import config
-from config import catalogue_path, config_name, num_ring, num_sector, num_neighbor_limit, region_r, num_false_star
+from config import catalogue_path, config_name, h, w, num_ring, num_sector, num_neighbor_limit, region_r, num_false_star
 from simulate import create_star_image
 from preprocess import get_star_centroids
 
 
-dataset_root_path = 'data'
 catalogue = pd.read_csv(catalogue_path, usecols= ["Star ID", "RA", "DE", "Magnitude"])
 num_input = num_ring + num_sector * num_neighbor_limit
 num_class = len(catalogue)
 
-# image dataset setting
-img_dataset_sub_path = os.path.join(dataset_root_path, f'star_images/{config_name}')
-# point dataset setting
-point_dataset_path = os.path.join(dataset_root_path, f'star_points/{config_name}')
+database_path = f'database/{config_name}'
+point_dataset_path = f'data/star_points/{config_name}'
+pattern_path = f'pattern/{config_name}'
 
 
-def get_rotation_angle(neighbor_star: np.ndarray, reference_star: np.ndarray) -> float:
+def generate_pattern_database(method: int, use_preprocess: bool = False, grid_len: int = 8):
     '''
-        Calculate the angle to rotate the neighbor star around reference star until it aligns to x axis.
+        Generate the pattern database for the given star catalogue.
     Args:
-        neighbor_star: the neighbor star
-        reference_star: the reference star
-    Returns:
-        angle: the angle to rotate the nearest neighbor star around reference star
+        method: the method to generate the pattern database
+                1: grid algorithm
+                2: radial and cyclic algorithm
+        use_preprocess: whether to avoid the error resulted from get_star_centroids function in preprocess stage
     '''
-    if neighbor_star[1] == reference_star[1]:
-        angle = 90
-        if neighbor_star[0] > reference_star[0]:
-            angle = 270
-    elif neighbor_star[0] == reference_star[0]:
-        angle = 0
-        if neighbor_star[1] < reference_star[1]:
-            angle = 180
-    else:
-        angle = abs(degrees(atan((neighbor_star[0] - reference_star[0]) / (neighbor_star[1] - reference_star[1]))))
-        # (-90, 90) -> (0, 360)
-        if neighbor_star[1] > reference_star[1]:
-            if neighbor_star[0] > reference_star[0]:
-                angle = 360 - angle
-        else:
-            if neighbor_star[0] < reference_star[0]:
-                angle = 180 - angle
-            else:
-                angle = 180 + angle
-    return angle
+    if not os.path.exists(database_path):
+        os.makedirs(database_path)
     
+    database = []
+    for ra, de in zip(catalogue['RA'], catalogue['DE']):
+        # star_info is a list of [star_ids[i], (row, col), star_magnitudes[i]]
+        img, star_info = create_star_image(degrees(ra), degrees(de), 0)
+        # generate star_table: (row, col) -> star_id
+        star_table = dict(map(lambda x: (x[1], x[0]), star_info))
+        # get the centroids of the stars in the image
+        if use_preprocess:
+            stars = np.array(get_star_centroids(img))
+        else:
+            stars = np.array(list(star_table.keys()))
 
-def generate_image_dataset(num_sample: int, h: int=224, w: int=224):
+        star_id = star_table.get((h/2, w/2), -1)
+        if star_id == -1:
+            print('The star is not in the center of the image!')
+            continue
+        # calculate the relative coordinates
+        stars = stars-(h/2, w/2)
+        # calculate the distance between the star and the center of the image
+        distances = np.linalg.norm(stars, axis=1)
+        # sort the stars by distance with accending order
+        stars = stars[distances.argsort()]
+        distances = np.sort(distances)
+        # exclude stars out of region
+        stars = stars[distances < region_r]
+        # exclude the reference star (h/2, w/2)
+        stars = stars[1:]
+        if len(stars) < 4:
+            continue
+        if method == 1:        
+            # find the nearest neighbor star
+            nearest_star = stars[0]
+            # calculate rotation angle & matrix
+            angle = np.arctan2(nearest_star[1], nearest_star[0])
+            M = np.array([[np.cos(angle), -np.sin(angle)], [np.sin(angle), np.cos(angle)]])
+            rotated_stars = np.dot(stars, M)
+            assert(round(rotated_stars[0][1])==0)
+            # calculate the pattern
+            grid_pattern = np.zeros((grid_len, grid_len), dtype=int)
+            for star in rotated_stars:
+                row = int(star[0]/region_r*grid_len)
+                col = int(star[1]/region_r*grid_len)
+                grid_pattern[row][col] = 1
+            binary_grid_pattern = ''.join(map(str, grid_pattern.flatten()))
+            database.append({
+                'pattern': binary_grid_pattern,
+                'id': star_id
+            })
+        elif method == 2:
+            # calculate the distance between the star and the center of the image
+            angles = np.arctan2(stars[:, 1], stars[:, 0])
+        else:
+            print('Invalid method!')
+
+    df = pd.DataFrame(database)
+    df.to_csv(f"{database_path}/db_{method}.csv", index=False)
+
+
+def generate_pattern_test_case(method: int, num_sample: int, use_preprocess: bool = False, grid_len: int = 8):
     '''
-        Generate the dataset from the given star catalogue.
-
-        Here's the brief introduction of the generation process. Select a guide star as the reference star. Then, find the closest neighbor star and rotate the image until the adjacent star lies on the horizontal axis. Finally, crop the image and save it.
-    Args:
-        num_sample: the number of samples to be generated
-        h: the width of the image
-        w: the length of the image
+        Generate pattern match test case.
     '''
-
-    dataset_path = os.path.join(dataset_root_path, img_dataset_sub_path)
-    if not os.path.exists(dataset_path):
-        os.makedirs(dataset_path)
-
-    # store the label information
-    labels = []
+    if not os.path.exists(pattern_path):
+        os.makedirs(pattern_path)
 
     # generate random right ascension[-180, 180] and declination[-90, 90]
     ras = np.random.uniform(-180, 180, num_sample)
     des = np.degrees(np.arcsin(np.random.uniform(-1, 1, num_sample)))
 
+    patterns = []
     # generate the star image
     for ra, de in zip(ras, des):
         # star_info is a list of [star_ids[i], (row, col), star_magnitudes[i]]
         img, star_info = create_star_image(ra, de, 0)
-        if len(star_info) < 3 + 1:
-            continue
         # generate star_table: (row, col) -> star_id
         star_table = dict(map(lambda x: (x[1], x[0]), star_info))
-
         # get the centroids of the stars in the image
-        stars = get_star_centroids(img)
-        # data structure for fast nearest neighbour search
-        tree = KDTree(stars)
+        if use_preprocess:
+            stars = np.array(get_star_centroids(img))
+        else:
+            stars = np.array(list(star_table.keys()))
 
-        stars = np.array(stars)
+        distances = cdist(stars, stars, 'euclidean')
+        angles = np.arctan2(stars[:, 1] - stars[:, 1][:, None], stars[:, 0] - stars[:, 0][:, None])
         # choose a guide star as the reference star
-        for guide_star in stars:
+        for star, ds, ags in zip(stars, distances, angles):
             # check if false star
-            star_id = star_table.get(tuple(guide_star), -1)
+            star_id = star_table.get(tuple(star), -1)
             if star_id == -1:
                 continue
-            # check if the guide star is too close to the edge
-            row1, row2 = int(guide_star[0] - h/2), int(guide_star[0] + h/2)
-            col1, col2 = int(guide_star[1] - w/2), int(guide_star[1] + w/2)
-            if row1 < 0 or row2 > img.shape[0] or col1 < 0 or col2 > img.shape[1]:
-                continue
-            
-            # find the top star_num_per_img nearest neighbor star to the reference one
-            distances, idxs = tree.query(guide_star, 3 + 1)
-            # make sure at least star_num_per_img neighbor stars are located in the region
-            if distances[-1] >= min(h, w) / 2:
+
+            # angles is sorted by distance with accending order
+            ss, ags = stars[np.argsort(ds)], ags[np.argsort(ds)]
+            ds = np.sort(ds)
+            ss, ags = ss[ds < region_r], ags[ds < region_r]
+            if len(ss) < 5:
                 continue
 
-            # find the nearest neighbor star
-            nearest_star = stars[idxs[1]]
-            # calculate rotation angle & matrix
-            angle = get_rotation_angle(nearest_star, guide_star)
-            # be careful that getRotationMatrix2D takes (width, height) as input, in other words (col, row)
-            M = cv2.getRotationMatrix2D((float(guide_star[1]), float(guide_star[0])), -angle, 1)
-            rotated_img = cv2.warpAffine(img, M, img.shape)
+            # remove the first element of ags & ds, which is reference star
+            assert(star[0] == ss[0][0] and star[1] == ss[0][1])
+            ss, ds, ags = ss[1:], ds[1:], ags[1:]
+            # calculate the relative coordinates
+            ss = ss-star
 
-            # crop and save the image
-            img_name = f'{datetime.now()}_{star_id}.png'
-            cv2.imwrite(os.path.join(dataset_path, img_name), rotated_img[row1:row2, col1:col2])
-            # generate label information for training and testing
-            labels.append({
-                'img_name': img_name,
-                'ra': ra,
-                'de': de,
-                'star_id': star_id
-            })
-
-    # save the label information
-    df = pd.DataFrame(labels)
-
-    # read the old label information
-    if os.path.exists(os.path.join(dataset_path, 'labels.csv')):
-        odf = pd.read_csv(os.path.join(dataset_path, 'labels.csv'), usecols=['img_name', 'ra', 'de', 'star_id'])
-        df = pd.concat([df, odf], ignore_index=True)
-    df.to_csv(os.path.join(dataset_path, 'labels.csv'))
+            if method == 1:
+                ag = ags[0]
+                M = np.array([[np.cos(ag), -np.sin(ag)], [np.sin(ag), np.cos(ag)]])
+                ss = np.dot(ss, M)
+                # calculate the pattern
+                grid_pattern = np.zeros((grid_len, grid_len), dtype=int)
+                for s in ss:
+                    row = int(s[0]/region_r*grid_len)
+                    col = int(s[1]/region_r*grid_len)
+                    grid_pattern[row][col] = 1
+                binary_grid_pattern = ''.join(map(str, grid_pattern.flatten()))
+                patterns.append({
+                    'pattern': binary_grid_pattern, 
+                    'id': star_id
+                })
+            elif method == 2:
+                pass
+            else:
+                pass
+    
+    df = pd.DataFrame(patterns)
+    df.to_csv(f"{pattern_path}/test_{method}.csv", index=False)
 
 
 def generate_point_dataset(type: str, num_sample: int):
@@ -205,7 +225,7 @@ def generate_point_dataset(type: str, num_sample: int):
             # remove the first element of ags & ds, which is reference star
             ds, ags = ds[1:], ags[1:]
             # make sure several neighbor stars are located in the region
-            if len(ags) < num_neighbor_limit:
+            if (config.type_noise == 'false_star' and len(ags) < num_neighbor_limit+num_false_star) or len(ags) < num_neighbor_limit:
                 continue
         
             ring_counts, _ = np.histogram(ds, bins=num_ring, range=(0, region_r))
@@ -236,48 +256,45 @@ if __name__ == '__main__':
     # all_task=[]
     # for i in range(num_thread):
     #     task = pool.submit(generate_point_dataset, 'train', 2000)
-    #     # if i%6 == 0:
+    #     # if i%3 == 0:
     #     #     task = pool.submit(generate_point_dataset, 'train', 2000)
-    #     # elif i%6 == 1:
+    #     # elif i%3 == 1:
     #     #     task = pool.submit(generate_point_dataset, 'validate', 2000)
-    #     # elif i%6 == 2:
+    #     # elif i%3 == 2:
     #     #     task = pool.submit(generate_point_dataset, 'test', 1000)
-    #     # elif i%6 == 3:
-    #     #     task = pool.submit(generate_point_dataset, 'positional_noise_test', 1000)
-    #     # elif i%6 == 4:
-    #     #     task = pool.submit(generate_point_dataset, 'magnitude_noise_test', 1000)
-    #     # else:
-    #     #     task = pool.submit(generate_point_dataset, 'false_star_test', 1000)
     #     all_task.append(task)
     # # wait for all tasks to be done
     # for task in all_task:
     #     task.result()
 
-    generate_point_dataset('false_star_test', 1000)
+    # generate_point_dataset('false_star_test', 1000)
     # generate_point_dataset('positional_noise_test', 1000)
     # generate_point_dataset('magnitude_noise_test', 1000)
 
-    # after all tasks are done, aggregate the dataset
-    for type in ['train', 'validate', 'test', 'positional_noise_test', 'magnitude_noise_test', 'false_star_test']:
-        dataset_path = os.path.join(point_dataset_path, type)
-        if type == 'false_star_test':
-            dataset_path = os.path.join(dataset_path, f'{num_false_star}')
-        files = os.listdir(dataset_path)
-        labels = pd.concat([pd.read_csv(os.path.join(dataset_path, file)) for file in files if file != 'labels.csv'], ignore_index=True)
-        labels_info = labels['star_id'].value_counts()
-        print(type, len(labels_info), labels_info.head(5), labels_info.tail(5))
+    # # after all tasks are done, aggregate the dataset
+    # for type in ['train', 'validate', 'test', 'positional_noise_test', 'magnitude_noise_test', 'false_star_test']:
+    #     dataset_path = os.path.join(point_dataset_path, type)
+    #     if type == 'false_star_test':
+    #         dataset_path = os.path.join(dataset_path, f'{num_false_star}')
+    #     files = os.listdir(dataset_path)
+    #     labels = pd.concat([pd.read_csv(os.path.join(dataset_path, file)) for file in files if file != 'labels.csv'], ignore_index=True)
+    #     labels_info = labels['star_id'].value_counts()
+    #     print(type, len(labels_info), labels_info.head(5), labels_info.tail(5))
         
-    # #     if type == 'train':
-    # #         labels = labels.groupby('star_id').head(70)
-    # #     elif type == 'validate':
-    # #         labels = labels.groupby('star_id').head(20)
-    # #     else:
-    # #         labels = labels.groupby('star_id').head(20)
+    #     if type == 'train':
+    #         labels = labels.groupby('star_id').head(70)
+    #     elif type == 'validate':
+    #         labels = labels.groupby('star_id').head(20)
+    #     else:
+    #         labels = labels.groupby('star_id').head(20)
 
-        labels.to_csv(f"{dataset_path}/labels.csv", index=False)
+    #     labels.to_csv(f"{dataset_path}/labels.csv", index=False)
     
-    # # for type in ['train', 'validate', 'test']:
-    # #     dataset_path = os.path.join(point_dataset_path, type)
-    # #     labels = pd.read_csv(os.path.join(dataset_path, 'labels.csv'))
-    # #     labels_info = labels['star_id'].value_counts()
-    # #     print(type, len(labels_info), labels_info.head(5), labels_info.tail(5))    
+    # for type in ['train', 'validate', 'test']:
+    #     dataset_path = os.path.join(point_dataset_path, type)
+    #     labels = pd.read_csv(os.path.join(dataset_path, 'labels.csv'))
+    #     labels_info = labels['star_id'].value_counts()
+    #     print(type, len(labels_info), labels_info.head(5), labels_info.tail(5))
+
+    generate_pattern_database(1)
+    generate_pattern_test_case(1, 100)
