78b5e0786947df8c84601ed069b45238846b814d
Zhytou <zhoytou@gmail.com>
2024-03-20 16:52:06 +0800

    feat: update generate & add aggregate_pattern_test 1、移除config，直接从simulate中import w, h, FOV, catalogue_path 并由此构成dataset、pattern以及database的sub path； 2、给generate函数增添pos_noise_std, mv_noise_std和num_false_star参数，用于生成不同的test，并将这些test保存在相应子文件夹中。

--- generate.py
+++ generate.py
@@ -6,22 +6,24 @@ import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
 from scipy.spatial.distance import cdist
 
-import config
-from config import catalogue_path, config_name, h, w, num_ring, num_sector, num_neighbor_limit, region_r, num_false_star
-from simulate import create_star_image
+from simulate import w, h, FOV, catalogue_path, create_star_image
 from preprocess import get_star_centroids
 
 
+# region radius in pixels
+region_r = int(w/FOV*6)
+
+# star catalogue
 catalogue = pd.read_csv(catalogue_path, usecols= ["Star ID", "RA", "DE", "Magnitude"])
-num_input = num_ring + num_sector * num_neighbor_limit
-num_class = len(catalogue)
 
-database_path = f'database/{config_name}'
-point_dataset_path = f'data/star_points/{config_name}'
-pattern_path = f'pattern/{config_name}'
+# define the path to store the database and pattern as well as dataset
+simulte_name = f"{os.path.basename(catalogue_path).rsplit('.', 1)[0]}_{w}x{h}_{FOV}x{FOV}"
+database_path = f'database/{simulte_name}'
+pattern_path = f'pattern/{simulte_name}'
+point_dataset_path = f'data/star_points/{simulte_name}'
 
 
-def generate_pattern_database(method: int, use_preprocess: bool = False, grid_len: int = 8):
+def generate_pattern_database(method: int, use_preprocess: bool = False, grid_len: int = 8, num_ring: int = 200, num_sector: int = 30):
     '''
         Generate the pattern database for the given star catalogue.
     Args:
@@ -29,6 +31,9 @@ def generate_pattern_database(method: int, use_preprocess: bool = False, grid_le
                 1: grid algorithm
                 2: radial and cyclic algorithm
         use_preprocess: whether to avoid the error resulted from get_star_centroids function in preprocess stage
+        grid_len: the length of the grid
+        num_ring: the number of rings
+        num_sector: the number of sectors
     '''
     if not os.path.exists(database_path):
         os.makedirs(database_path)
@@ -93,27 +98,45 @@ def generate_pattern_database(method: int, use_preprocess: bool = False, grid_le
             # get the radial and cyclic features
             ring_counts, _ = np.histogram(distances, bins=num_ring, range=(0, region_r))
             sector_counts, _ = np.histogram(angles, bins=num_sector, range=(-np.pi, np.pi))
-            pattern = ''.join(map(str, ring_counts))#np.concatenate([ring_counts, sector_counts])))
-            record = {
-                'pattern': pattern,
+            database.append({
+                'pattern': ''.join(map(str, np.concatenate([ring_counts, sector_counts]))),
                 'id': star_id
-            }
-            for i, d in enumerate(distances):
-                record[f'd_{i}'] = d
-            database.append(record)
+            })
         else:
             print('Invalid method!')
+            return
 
     df = pd.DataFrame(database)
-    df.to_csv(f"{database_path}/db_{method}.csv", index=False)
+    if method == 1:
+        df.to_csv(f"{database_path}/db{method}_{grid_len}x{grid_len}.csv", index=False)
+    else:
+        df.to_csv(f"{database_path}/db{method}_{num_ring}_{num_sector}.csv", index=False)
 
 
-def generate_pattern_test_case(method: int, num_sample: int, use_preprocess: bool = False, grid_len: int = 8):
+def generate_pattern_test_case(method: int, num_sample: int, use_preprocess: bool = False, grid_len: int = 8, num_ring: int = 200, num_sector: int = 30, pos_noise_std: int = 0, mv_noise_std: float = 0, num_false_star: int = 0):
     '''
         Generate pattern match test case.
+    Args:
+        method: the method to generate the pattern
+                1: grid algorithm
+                2: radial and cyclic algorithm
+        use_preprocess: whether to avoid the error resulted from get_star_centroids function in preprocess stage
+        grid_len: the length of the grid
+        num_ring: the number of rings
+        num_sector: the number of sectors
+        pos_noise_std: the standard deviation of the positional noise
+        mv_noise_std: the standard deviation of the magnitude noise
+        num_false_star: the number of false stars
     '''
-    if not os.path.exists(pattern_path):
-        os.makedirs(pattern_path)
+    if method == 1:
+        pattern_test_path = os.path.join(pattern_path, f'db{method}_{grid_len}x{grid_len}_pos{pos_noise_std}_mv{mv_noise_std}_fs{num_false_star}_test')
+    elif method == 2:
+        pattern_test_path = os.path.join(pattern_path, f'db{method}_{num_ring}_{num_sector}_pos{pos_noise_std}_mv{mv_noise_std}_fs{num_false_star}_test')
+    else:
+        print('Invalid method!')
+        return
+    if not os.path.exists(pattern_test_path):
+        os.makedirs(pattern_test_path)
 
     # generate random right ascension[-180, 180] and declination[-90, 90]
     ras = np.random.uniform(-180, 180, num_sample)
@@ -123,7 +146,7 @@ def generate_pattern_test_case(method: int, num_sample: int, use_preprocess: boo
     # generate the star image
     for ra, de in zip(ras, des):
         # star_info is a list of [star_ids[i], (row, col), star_magnitudes[i]]
-        img, star_info = create_star_image(ra, de, 0)
+        img, star_info = create_star_image(ra, de, 0, pos_noise_std=pos_noise_std, mv_noise_std=mv_noise_std, num_false_star=num_false_star)
         # generate star_table: (row, col) -> star_id
         star_table = dict(map(lambda x: (x[1], x[0]), star_info))
         # get the centroids of the stars in the image
@@ -173,7 +196,7 @@ def generate_pattern_test_case(method: int, num_sample: int, use_preprocess: boo
                     'pattern': ''.join(map(str, grid_pattern.flatten())), 
                     'id': star_id
                 })
-            elif method == 2:
+            else:
                 # rotate the stars until the nearest star lies on the horizontal axis
                 ags = ags-ags[0]
                 # make sure angles are in the range of [-pi, pi]
@@ -183,39 +206,30 @@ def generate_pattern_test_case(method: int, num_sample: int, use_preprocess: boo
                 # get the radial and cyclic features
                 ring_counts, _ = np.histogram(ds, bins=num_ring, range=(0, region_r))
                 sector_counts, _ = np.histogram(ags, bins=num_sector, range=(-np.pi, np.pi))
-                pattern = ''.join(map(str, ring_counts))#np.concatenate([ring_counts, sector_counts])))
-                record = {
-                    'pattern': pattern,
+                patterns.append({
+                    'pattern': ''.join(map(str, np.concatenate([ring_counts, sector_counts]))),
                     'id': star_id
-                }
-                for i, d in enumerate(ds):
-                    record[f'd_{i}'] = d
-                patterns.append(record)
-            else:
-                pass
-    
+                })
+            
     df = pd.DataFrame(patterns)
-    df.to_csv(f"{pattern_path}/test_{method}.csv", index=False)
+    df.to_csv(os.path.join(pattern_test_path, str(uuid.uuid1())),index=False)
 
 
-def generate_point_dataset(type: str, num_sample: int):
+def generate_point_dataset(type: str, num_sample: int, num_ring: int = 200, num_sector: int = 30, pos_noise_std: int = 0, mv_noise_std: float = 0, num_false_star: int = 0, num_neighbor_limit: int = 4):
     '''
         Generate the dataset from the given star catalogue.
     Args:
-        type: 'train', 'validate', 'test', 'positional_noise_test', 'magnitude_noise_test', 'false_star_test'
+        type: 'train', 'validate', 'test'
         num_sample: the number of samples to be generated
+        num_ring: the number of rings
+        num_sector: the number of sectors
+        pos_noise_std: the standard deviation of the positional noise
+        mv_noise_std: the standard deviation of the magnitude noise
+        num_false_star: the number of false stars
     '''
-    
-    dataset_path = os.path.join(point_dataset_path, type)
-    if type == 'positional_noise_test':
-        config.type_noise = 'pos'
-    elif type == 'magnitude_noise_test':
-        config.type_noise = 'mv'
-    elif type == 'false_star_test':
-        config.type_noise = 'false_star'
-        dataset_path = os.path.join(dataset_path, f'{num_false_star}')
-    else:
-        config.type_noise = 'none'
+    dataset_path = os.path.join(point_dataset_path, f'{num_ring}_{num_sector}_{num_neighbor_limit}', type)
+    if type == 'test':
+        database_path = os.path.join(database_path, f'pos{pos_noise_std}_mv{mv_noise_std}_fs{num_false_star}_test')
     if not os.path.exists(dataset_path):
         os.makedirs(dataset_path)
 
@@ -229,7 +243,7 @@ def generate_point_dataset(type: str, num_sample: int):
     # generate the star image
     for ra, de in zip(ras, des):
         # star_info is a list of [star_ids[i], (row, col), star_magnitudes[i]]
-        img, star_info = create_star_image(ra, de, 0)
+        img, star_info = create_star_image(ra, de, 0, pos_noise_std=pos_noise_std, mv_noise_std=mv_noise_std, num_false_star=num_false_star)
         # generate star_table: (row, col) -> star_id
         star_table = dict(map(lambda x: (x[1], x[0]), star_info))
         # get the centroids of the stars in the image
@@ -262,7 +276,7 @@ def generate_point_dataset(type: str, num_sample: int):
             # remove the first element of ags & ds, which is reference star
             ds, ags = ds[1:], ags[1:]
             # make sure several neighbor stars are located in the region
-            if (config.type_noise == 'false_star' and len(ags) < num_neighbor_limit+num_false_star) or len(ags) < num_neighbor_limit:
+            if len(ags) < num_neighbor_limit+num_false_star:
                 continue
         
             ring_counts, _ = np.histogram(ds, bins=num_ring, range=(0, region_r))
@@ -285,53 +299,111 @@ def generate_point_dataset(type: str, num_sample: int):
     df.to_csv(os.path.join(dataset_path, str(uuid.uuid1())), index=False)
 
 
-if __name__ == '__main__':
-    # num_thread = 6
-    # # use thread pool
-    # pool = ThreadPoolExecutor(max_workers=num_thread)
-    # # generate dataset
-    # all_task=[]
-    # for i in range(num_thread):
-    #     task = pool.submit(generate_point_dataset, 'train', 2000)
-    #     # if i%3 == 0:
-    #     #     task = pool.submit(generate_point_dataset, 'train', 2000)
-    #     # elif i%3 == 1:
-    #     #     task = pool.submit(generate_point_dataset, 'validate', 2000)
-    #     # elif i%3 == 2:
-    #     #     task = pool.submit(generate_point_dataset, 'test', 1000)
-    #     all_task.append(task)
-    # # wait for all tasks to be done
-    # for task in all_task:
-    #     task.result()
+def aggregate_point_dataset(num_round: int, types: list = ['train', 'validate', 'test'], num_sample: int = 1000, num_ring: int = 200, num_sector: int = 30, pos_noise_stds: list = [0, 1, 2], mv_noise_stds: list = [0, 0.1, 0.2], num_false_stars: list = [0, 1, 2, 3, 4, 5], num_neighbor_limit: int = 4):
+    '''
+        Aggregate the dataset.
+    Args:
+        num_round: the number of rounds to generate the dataset
+        types: the types of the dataset
+        num_ring: the number of rings
+        num_sector: the number of sectors
+        num_neighbor_limit: the number of neighbor stars
+    '''
+    # use thread pool
+    num_thread = num_round*len(types)
+    pool = ThreadPoolExecutor(max_workers=num_thread)
+    # generate dataset
+    all_task=[]
+    for i in range(num_thread):
+        task = pool.submit(generate_point_dataset, 'train', num_sample, num_ring, num_sector)
+        all_task.append(task)
+    # wait for all tasks to be done
+    for task in all_task:
+        task.result()
 
-    # generate_point_dataset('false_star_test', 1000)
-    # generate_point_dataset('positional_noise_test', 1000)
-    # generate_point_dataset('magnitude_noise_test', 1000)
-
-    # # after all tasks are done, aggregate the dataset
-    # for type in ['train', 'validate', 'test', 'positional_noise_test', 'magnitude_noise_test', 'false_star_test']:
-    #     dataset_path = os.path.join(point_dataset_path, type)
-    #     if type == 'false_star_test':
-    #         dataset_path = os.path.join(dataset_path, f'{num_false_star}')
-    #     files = os.listdir(dataset_path)
-    #     labels = pd.concat([pd.read_csv(os.path.join(dataset_path, file)) for file in files if file != 'labels.csv'], ignore_index=True)
-    #     labels_info = labels['star_id'].value_counts()
-    #     print(type, len(labels_info), labels_info.head(5), labels_info.tail(5))
+    # after all tasks are done, aggregate the dataset
+    for type in ['train', 'validate', 'test', 'positional_noise_test', 'magnitude_noise_test', 'false_star_test']:
+        dataset_path = os.path.join(point_dataset_path, type)
         
-    #     if type == 'train':
-    #         labels = labels.groupby('star_id').head(70)
-    #     elif type == 'validate':
-    #         labels = labels.groupby('star_id').head(20)
-    #     else:
-    #         labels = labels.groupby('star_id').head(20)
+        files = os.listdir(dataset_path)
+        df = pd.concat([pd.read_csv(os.path.join(dataset_path, file)) for file in files if file != 'labels.csv'], ignore_index=True)
+        df_info = df['star_id'].value_counts()
+        print(type, len(df_info), df_info.head(5), df_info.tail(5))
 
-    #     labels.to_csv(f"{dataset_path}/labels.csv", index=False)
+        df.to_csv(f"{dataset_path}/labels.csv", index=False)
+
+
+def aggregate_pattern_test(num_round: int, methods: list = [1, 2], num_sample: int = 1000, grid_len: int = 8, num_ring: int = 200, num_sector: int = 30, pos_noise_stds: list = [0, 1, 2], mv_noise_stds: list = [0, 0.1, 0.2], num_false_stars: list = [0, 1, 2, 3, 4, 5]):
+    '''
+        Aggregate the pattern test.
+    Args:
+        num_round: the number of rounds to generate the test
+                    if num_round == 0: only aggregate the test cases
+        methods: the methods to generate the pattern
+        num_sample: the number of samples to be generated
+        grid_len: the length of the grid
+        num_ring: the number of rings
+        num_sector: the number of sectors
+        pos_noise_stds: the standard deviations of the positional noise
+        mv_noise_stds: the standard deviations of the magnitude noise
+        num_false_stars: the number of false stars
+    '''
+
+    def merge_test(test_path: str):
+        '''
+            Merge the test cases into a single file.
+        Args:
+            path: the path to the test cases
+        '''
+        files = os.listdir(test_path)
+        df = pd.concat([pd.read_csv(os.path.join(test_path, file)) for file in files if file != 'test.csv'], ignore_index=True)
+        df.to_csv(f"{test_path}/test.csv", index=False)
+
+    # use thread pool
+    num_thread = len(pos_noise_stds)+len(mv_noise_stds)+len(num_false_stars)
+    pool = ThreadPoolExecutor(max_workers=num_thread)
+    # iterate to generate the test cases
+    all_task = []
+    for _ in range(num_round):
+        for method in methods:
+            if method > 2:
+                continue
+            for pos_noise_std in pos_noise_stds:
+                task = pool.submit(generate_pattern_test_case, method, num_sample, grid_len=grid_len, um_ring=num_ring, num_sector=num_sector, pos_noise_std=pos_noise_std)
+                all_task.append(task)
+            for mv_noise_std in mv_noise_stds:
+                task = pool.submit(generate_pattern_test_case, method, num_sample, grid_len=grid_len, num_ring=num_ring, num_sector=num_sector, mv_noise_std=mv_noise_std)
+                all_task.append(task)
+            for num_false_star in num_false_stars:    
+                task = pool.submit(generate_pattern_test_case, method, num_sample, grid_len=grid_len, num_ring=num_ring, num_sector=num_sector, num_false_star=num_false_star)
+                all_task.append(task)
     
-    # for type in ['train', 'validate', 'test']:
-    #     dataset_path = os.path.join(point_dataset_path, type)
-    #     labels = pd.read_csv(os.path.join(dataset_path, 'labels.csv'))
-    #     labels_info = labels['star_id'].value_counts()
-    #     print(type, len(labels_info), labels_info.head(5), labels_info.tail(5))
+    # wait for all task done
+    for task in all_task:
+        task.result()
+    
+    # aggregate the test
+    for method in methods:
+        if method == 1:
+            for pos_noise_std in pos_noise_stds:
+                merge_test(os.path.join(pattern_path, f'db{method}_{grid_len}x{grid_len}_pos{pos_noise_std}_mv{0}_fs{0}_test'))
+            for mv_noise_std in mv_noise_stds:
+                merge_test(os.path.join(pattern_path, f'db{method}_{grid_len}x{grid_len}_pos{0}_mv{mv_noise_std}_fs{0}_test'))
+            for num_false_star in num_false_stars:
+                merge_test(os.path.join(pattern_path, f'db{method}_{grid_len}x{grid_len}_pos{0}_mv{0}_fs{num_false_star}_test'))
+        elif method == 2:
+            for pos_noise_std in pos_noise_stds:
+                merge_test(os.path.join(pattern_path, f'db{method}_{num_ring}_{num_sector}_pos{pos_noise_std}_mv{0}_fs{0}_test'))
+            for mv_noise_std in mv_noise_stds:
+                merge_test(os.path.join(pattern_path, f'db{method}_{num_ring}_{num_sector}_pos{0}_mv{mv_noise_std}_fs{0}_test'))
+            for num_false_star in num_false_stars:
+                merge_test(os.path.join(pattern_path, f'db{method}_{num_ring}_{num_sector}_pos{0}_mv{0}_fs{num_false_star}_test'))
+        else:
+            continue
+    
 
-    generate_pattern_database(1, grid_len=60)
-    generate_pattern_test_case(1, 500, grid_len=60)
+if __name__ == '__main__':
+    # generate_pattern_database(1, grid_len=60)
+    # generate_pattern_database(2)
+    aggregate_pattern_test(0, [2], grid_len = 60, num_sample=100)
+    
