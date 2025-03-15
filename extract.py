32c11463666e792b5160cb073e6755deffd787df
Zhytou <zhoytou@gmail.com>
2025-03-15 17:14:35 +0800

    remove IWCoG

--- extract.py
+++ extract.py
@@ -57,7 +57,7 @@ def cal_center_of_guassian_curve(img: np.ndarray, rows, cols) -> tuple[float, fl
     return round(-X[1]/(2*X[0]), 3), round(-X[2]/(2*X[0]), 3)
 
 
-def cal_center_of_gravity(img: np.ndarray, rows: np.ndarray, cols: np.ndarray, method: str, T: int=-1, center: tuple[int, int]=None, A: float=200, sigma: float=1.0, n: int=1) -> tuple[float, float]:
+def cal_center_of_gravity(img: np.ndarray, rows: np.ndarray, cols: np.ndarray, method: str, T: int=-1, center: tuple[int, int]=None, A: float=200, sigma: float=1.0) -> tuple[float, float]:
     '''
         Calculate the centroid of the star in the window.
     Args:
@@ -67,48 +67,39 @@ def cal_center_of_gravity(img: np.ndarray, rows: np.ndarray, cols: np.ndarray, m
             'CoG': center of gravity
             'MCoG': modified center of gravity
             'WCoG': weighed center of gravity
-            'IWCoG': iterative weighed center of gravity
+            # 'IWCoG': iterative weighed center of gravity
         T: the threshold of the star
-        center: initial centroid used in WCoG and IWCoG
+        center: initial centroid used in WCoG
         sigma: the sigma used in WCoG and IWCoG
     Returns:
         centroid: the centroid of the star
     '''
-    # number of iterations for methods except 'IWCoG' is set to 1
-    if method != 'IWCoG':
-        n = 1
-
-    # move the initial centroid to the center of the pixel
-    if center is not None:
-        center = center[0]+0.5, center[1]+0.5
     
     # gray
     g = img[rows, cols]
     # row and column add 0.5 to get the center of the pixel
     x, y = rows+0.5, cols+0.5
 
-    # iterate n times
-    while n > 0:
-        n -= 1
-        if method == 'CoG':
-            # row multiply gray and sum, column multiply gray and sum
-            xgs, ygs = np.sum(x * g), np.sum(y * g)
-            gs = np.sum(g)
-        elif method == 'MCoG':
-            xgs, xgs = np.sum(x * (g - T)), np.sum(y * (g - T))
-            gs = np.sum(g - T)
-        elif method == 'WCoG' or method == 'IWCoG':
-            # weight for each pixel used in WCoG and IWCoG
-            d = (x-center[0])**2 + (y-center[1])**2
-            w = A*np.exp(-d/(2*sigma**2))
-            xgs, ygs = np.sum(x * g * w), np.sum(y * g * w)
-            gs = np.sum(g * w)
-            if gs == 0.0:
-                return 0.0, 0.0
-        else:
-            print('wrong gravity method!')
+    if method == 'CoG':
+        # row multiply gray and sum, column multiply gray and sum
+        xgs, ygs = np.sum(x * g), np.sum(y * g)
+        gs = np.sum(g)
+    elif method == 'MCoG':
+        xgs, xgs = np.sum(x * (g - T)), np.sum(y * (g - T))
+        gs = np.sum(g - T)
+    elif method == 'WCoG':
+        # weight for each pixel used in WCoG and IWCoG
+        d = (x-center[0])**2 + (y-center[1])**2
+        w = A*np.exp(-d/(2*sigma**2))
+        xgs, ygs = np.sum(x * g * w), np.sum(y * g * w)
+        gs = np.sum(g * w)
+        if gs == 0.0:
             return 0.0, 0.0
-        center = round(xgs/gs, 3), round(ygs/gs, 3)
+    else:
+        print('wrong gravity method!')
+        return 0.0, 0.0
+    
+    center = round(xgs/gs, 3), round(ygs/gs, 3)
     return center
 
 
