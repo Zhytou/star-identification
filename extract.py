a4a036a77495ad43805b4a6574bcd8140aad2685
Zhytou <zhoytou@gmail.com>
2026-02-04 17:17:36 +0800

    update extract 更新cal_center_of_gravity函数，增加阈值内部计算功能

--- extract.py
+++ extract.py
@@ -32,37 +32,43 @@ def cal_center_of_guassian_curve(img: np.ndarray, rows, cols) -> tuple[float, fl
     return round(-X[1]/(2*X[0]), 3), round(-X[2]/(2*X[0]), 3)
 
 
-def cal_center_of_gravity(img: np.ndarray, rows: np.ndarray, cols: np.ndarray, method: str, T: float=0, A: float=200, sigma: float=1.0) -> tuple[float, float]:
+def cal_center_of_gravity(img: np.ndarray, rows: np.ndarray, cols: np.ndarray, cen_meth: str, thr_meth: str='Liebe3', T: float=None, A: float=200, size: int=15, sigma: float=1.0) -> tuple[float, float]:
     '''
         Calculate the centroid of the star in the window.
     '''
-    
-    y, x = rows, cols
-    g = img[y, x]
-    y, x = y + 0.5, x + 0.5
 
-    if method == 'CoG' or method == 'CCoG':
+    h, w = img.shape
+    r = size // 2
+
+    g = img[rows, cols]
+    y, x = rows, cols
+    x0, y0 = x[np.argmax(g)], y[np.argmax(g)]
+    if T is None:
+        y1, y2 = max(0, y0 - r), min(h - 1, y0 + r + 1)
+        x1, x2 = max(0, x0 - r), min(w - 1, x0 + r + 1)
+        T = cal_threshold(img[y1:y2, x1:x2], thr_meth)
+
+    if cen_meth == 'CoG' or cen_meth == 'CCoG':
         xgs, ygs = np.sum(x * g), np.sum(y * g)
         gs = np.sum(g)
-    elif method == 'MCoG':
+    elif cen_meth == 'MCoG':
         xgs, ygs = np.sum(x * (g - T)), np.sum(y * (g - T))
         gs = np.sum(g - T)
-    elif method == 'WCoG':
-        i = np.argmax(g)
-        x0, y0 = x[i], y[i]
+        print(xgs, ygs, gs)
+    elif cen_meth == 'WCoG':
         d2 = (x - x0)**2 + (y - y0)**2
-        w = A*np.exp(-d2 / (2 * sigma**2))
-        xgs, ygs = np.sum(x * g * w), np.sum(y * g * w)
-        gs = np.sum(g * w)
+        wt = A*np.exp(-d2 / (2 * sigma**2))
+        xgs, ygs = np.sum(x * g * wt), np.sum(y * g * wt)
+        gs = np.sum(g * wt)
     else:
-        print('Invalid gravity method!')
+        print('Invalid gravity cen_meth!')
         return 0.0, 0.0
     
     gs = np.maximum(gs, 1e-10)
-    return ygs/gs, xgs/gs, gs
+    return ygs / gs + 0.5, xgs / gs + 0.5, gs
 
 
-def get_star_centroids(img: np.ndarray, den_meth: str, seg_meth: list['str'], cen_meth: str, pixel_limit: int=5, connectivity=4, need_gray: bool=False) -> np.ndarray:
+def get_star_centroids(img: np.ndarray, den_meth: str, seg_meth: list['str'], cen_meth: str, size: int | list[int], pixel_limit: int=5, connectivity=4, gray: bool=False, output_dir: str=None) -> np.ndarray:
     '''
         Get the centroids of the stars in the image.
     '''
@@ -71,15 +77,14 @@ def get_star_centroids(img: np.ndarray, den_meth: str, seg_meth: list['str'], ce
     filtered_img = denoise_image(img, den_meth)
     
     # rough group star using connectivity
-    group_coords = group_star(filtered_img, seg_meth, connectivity=connectivity, pixel_limit=pixel_limit)
+    group_coords = group_star(filtered_img, seg_meth, size, connectivity=connectivity, pixel_limit=pixel_limit, output_dir=output_dir)
 
     # calculate the centroid coordinate with threshold and weight
-    T = cal_threshold(filtered_img, seg_meth[1])
-    centroids = np.array([cal_center_of_gravity(filtered_img, rows, cols, cen_meth, T) for rows, cols in group_coords])
+    centroids = np.array([cal_center_of_gravity(filtered_img, rows, cols, cen_meth) for rows, cols in group_coords]) if len(group_coords) > 0 else np.zeros((0, 3))
 
     # sort by star luminosity
     centroids = centroids[np.argsort(centroids[:, 2])][::-1]
-    if not need_gray:
+    if not gray:
         centroids = centroids[:, :2]
 
     return centroids
